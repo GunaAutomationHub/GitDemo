10cc4dbfebbec48ef61cf269d26bb11ee92574c2
GunaAutomationHub <gunami3@gmail.com>
2022-10-05 23:28:21 +0530

    American guy commit

--- src/test/java/demomaven/AppTest.java
+++ src/test/java/demomaven/AppTest.java
@@ -21,6 +21,10 @@ public class AppTest
         System.out.println("postJira1");
         System.out.println("postJira2");
         System.out.println("postJira3");
+        
+        System.out.println("postJira4");
+        System.out.println("postJira5");
+        System.out.println("postJira6");
     }
 
     /**
